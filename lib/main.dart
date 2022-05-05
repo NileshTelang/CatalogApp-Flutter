8276ba07f89b76d82c10401c8e82c0b420fe13fc
Nilesh Telang <nileshtelang07n5@gmail.com>
2022-05-05 14:42:50 +0530

    com commit

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class nova extends StatelessWidget {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: "/home",
+      initialRoute: "/login",
       routes: {
         MyRoutes.LogIn: (context) => logInPage(),
         MyRoutes.Home: (context) => HomePage(),
